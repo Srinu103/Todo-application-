1c2a55cfe03ad9e3d2d7741428ba9830293e6c2e
Srinu103 <141472783+Srinu103@users.noreply.github.com>
2023-08-05 14:43:13 +0530

    Update index.js

--- controller/index.js
+++ controller/index.js
@@ -1,1 +1,37 @@
+const date = require("../date.js");
 
+var items = []; // Variables serving as local database instead of an actual database
+var workÃ­tems = [];
+
+async function getTodoList (req, res) {
+    const today = date.getDate();
+    const pageData = {
+        listTitle: today,
+        newListItems: items
+      };
+
+  res.render("list", pageData); //render from the list of view folder
+}
+
+async function createTodoItem (req, res) {
+    const item = req.body.newItem;
+    if (req.body.list && req.body.list === "Work") {
+        workÃ­tems.push(item);
+        res.redirect("/work");
+    } else {
+        items.push(item);
+        res.redirect("/");
+    }
+
+}
+
+async function getWorkList (req, res) {
+    const pageData = {
+        listTitle: "Work List",
+        newListItems: workÃ­tems
+    };
+
+    res.render("list", pageData);
+}
+
+module.exports = {getTodoList, getWorkList, createTodoItem};
