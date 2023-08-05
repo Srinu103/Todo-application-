83044db5d23ed3a3f8f1aabaaaf195c15366c853
Srinu103 <141472783+Srinu103@users.noreply.github.com>
2023-08-05 15:43:13 +0530

    Update Index.js

--- Routes/Index.js
+++ Routes/Index.js
@@ -1,1 +1,14 @@
 
+const express = require('express');
+const app = express();
+const controllers = require('../controllers');
+const middlewares = require('../middlewares');
+
+// Mounting routes with middlewares and controllers
+app.get("/", controllers.getTodoList)
+
+app.post("/", middlewares.validateRequestBody, controllers.createTodoItem);
+
+app.get("/work", controllers.getWorkList);
+
+module.exports = app;
